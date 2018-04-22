080a5b5450004bbc27c9e7f731be2e4a48d51515
Isaac343 <izaak343@gmail.com>
2018-04-21 19:56:57 -0500

    3

--- inc/view/login.php
+++ inc/view/login.php
@@ -1,24 +1,6 @@
-<link rel="stylesheet" href="../elements/css/login.css">
-<div class="content">
-  <div class="container">
-    <div class="row">
-      <div class="col special">
-        <center><img src="../elements/img/logo-white.png" alt="" height="30"></center>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-4 mx-auto">
-        <form>
-          <h1><i class="icons">person_pin</i> Login</h1>
-          <div class="form-group">
-            <input type="email" placeholder="Username" name="username">
-          </div>
-          <div class="form-group">
-            <input type="password" placeholder="Password" name="password">
-          </div>
-          <button type="submit" name="submit" class="btn btn-primary">Login</button>
-        </form>
-      </div>
-    </div>
+<link rel="stylesheet" href="/src/css/master.css">
+<div class="container">
+  <div class="second_container">
+
   </div>
 </div>
